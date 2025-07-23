ae67f55c51c26e643eca4b3f4c53f98a1b1c4ae4
Maimouna <sokhnamaimounafaye758@gmail.com>
2025-07-23 22:07:49 +0000

    scripts

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,24 +1,20 @@
-// Fonction pour ajouter un produit au panier
 document.querySelectorAll('.ajout-panier').forEach(button => {
     button.addEventListener('click', function(){
         const productId = this.dataset.id;
-        const quantity = 1; // Par défaut, on ajoute 1 produit
+        const quantity = 1; 
         
 
         let panier = JSON.parse(localStorage.getItem('panier')) || [];
 
-        // Vérifier si le produit est déjà dans le panier
         const productInCart = panier.find(item => item.id === productId);
         if (productInCart) {
-            productInCart.quantite += quantity; // Si produit existant, on incrémente la quantité
+            productInCart.quantite += quantity; 
         } else {
-            panier.push({ id: productId, quantite: quantity }); // Sinon, on l'ajoute au panier
+            panier.push({ id: productId, quantite: quantity }); 
         }
 
-        // Sauvegarder le panier dans le localStorage
         localStorage.setItem('panier', JSON.stringify(panier));
 
-        // Afficher une alerte ou un message de confirmation
         alert('Produit ajouté au panier !');
     });
 });
